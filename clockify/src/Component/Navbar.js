213f0f79368d8cb19e9b5cf87a213b133d49fd94
pavan <pavanambulkar69@gmail.com>
2022-09-28 04:20:09 +0530

    do

--- clockify/src/Component/Navbar.js
+++ clockify/src/Component/Navbar.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Img, Text } from "@chakra-ui/react";
+import { Box, Flex, Img, } from "@chakra-ui/react";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
