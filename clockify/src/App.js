69728ff48a7bc25a2351d6482e8dc0d7938912e9
saurabh solanki <solankisourabh25@gmail.com>
2022-09-28 22:27:02 +0530

    n

--- clockify/src/App.js
+++ clockify/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Component/Navbar'
+import TimeTracker from './Dashboard/TimeTracker';
 import AllRoutes from './Routes/AllRoutes';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <AllRoutes/>
+      <TimeTracker/>
     </div>
   );
 }
--- clockify/src/Dashboard/TimeTracker.jsx
+++ clockify/src/Dashboard/TimeTracker.jsx
@@ -1,42 +1,111 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useCountdownTimer } from 'use-countdown-timer';
-
+import React, { useEffect, useRef, useState } from "react";
+import { useCountdownTimer } from "use-countdown-timer";
 
 const TimeTracker = () => {
+  const [data, setData] = useState([]);
 
-    const [count, setCount] = useState(0);
-    const timeref = useRef(null);
+  const [text, setText] = useState("");
+  const [select, setSelect] = useState();
+  const [startTime, setStartTime] = useState();
+  const [endTime, setEndTime] = useState();
 
-    useEffect(() => {
-      return stopTime;
-    }, []);
-  
-    const startTime = () => {
-      if (timeref.current !== null) return;
-      timeref.current = setInterval(() => {
-        setCount((count) => count + 1);
-      }, 1000);
-    };
-    const stopTime = () => {
-        clearInterval(timeref.current);
-        timeref.current = null;
-      };
-    
+  let arr = new Date().toTimeString().split(" ");
+  let time = arr[0].split(":");
+  let [hours, minutes] = time;
+
+  const [timer, setTimer] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const countRef = useRef(null);
+
+  const handleStart = () => {
+    // start button logic here
+    setIsActive(true);
+    setIsPaused(true);
+    countRef.current = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+    setStartTime(`${hours}-${minutes}`);
+  };
+
+  const handlePause = () => {
+    // Pause button logic here
+    clearInterval(countRef.current);
+    setIsPaused(false);
+    setIsActive(true);
+    setEndTime(`${hours}-${minutes}`);
+    setData([...data, showData]);
+    setIsPaused(true);
+  };
+
+  const formatTime = () => {
+    const getSeconds = `0${timer % 60}`.slice(-2);
+    const minutes = `${Math.floor(timer / 60)}`;
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2);
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  };
+
+  let showData = {
+    text: text,
+    select: select,
+    StartTime: startTime,
+    endTime: endTime,
+    timer: formatTime(),
+  };
+
   return (
-    <div>
-      <form action="">
-        <input type="text" placeholder='What are you working on' />
-        <select name="Project" id="">
-            <option value="React"></option>
-            <option value="Redux"></option>
-            <option value="Thunk"></option>
-        </select>
-        <button>tag</button>
-        <p>Timer</p>
-        <button type='submit'>Start</button>
-      </form>
-    </div>
-  )
-}
+    <div id="main_container">
+      <div style={{ display: "flex", border: "1px solid black", gap: "10px" }}>
+        <div>
+          <input
+            type="text"
+            placeholder="What are you working on"
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div>
+          <select
+            name="Project"
+            id=""
+            onChange={(e) => setSelect(e.target.value)}
+          >
+            <option value="React">Project</option>
+            <option value="Redux">Redux</option>
+            <option value="Thunk">Thunk</option>
+          </select>
+        </div>
+        <div>
+          {" "}
+          <button>tag</button>
+        </div>
+        <div>
+          <p>{formatTime()}</p>
+        </div>
+        <div>
+          {!isActive && !isPaused ? (
+            <button onClick={handleStart}>Start</button>
+          ) : (
+            <button onClick={handlePause}>
+              {isPaused ? "Pause" : "Start"}
+            </button>
+          )}
+        </div>
+      </div>
 
-export default TimeTracker
+      <div id="data_show_container">
+        {data.map((e) => (
+          <div style={{display:"flex", gap:"20px"}}>
+            <p>{e.text}</p>
+            <p>{e.select}</p>
+            <p>{e.StartTime}--</p>
+            <p>{e.endTime}</p>
+            <p>{e.timer}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TimeTracker;
