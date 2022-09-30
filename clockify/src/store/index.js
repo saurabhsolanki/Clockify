75b81ad54b83092b0096cd6b60e85c4816642564
arulxavierax <arulxavier2121@gmail.com>
2022-09-30 23:52:50 +0530

    Added project and team section files

--- clockify/src/store/index.js
+++ clockify/src/store/index.js
@@ -3,11 +3,15 @@ import thunk from "redux-thunk";
 import { authReducer } from "./Auth/authReducer";
 import { isOpen } from "./isOpen/isOpen.Reducer";
 import { TimeTrackerReducer } from "./Timer/TimeReducer";
+import { teamReducer } from "./Team/team.reducer";
+import { teamReducer } from "./Project/project.reducer";
 
 const rootReducer = combineReducers({
   time: TimeTrackerReducer,
   auth: authReducer,
   checkOpen: isOpen,
+  team: teamReducer,
+  project: teamReducer,
 });
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
