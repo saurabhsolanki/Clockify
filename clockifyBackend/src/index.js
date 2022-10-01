9549e569097b6d6bb94e9b6197696cbb290d7667
saurabh solanki <solankisourabh25@gmail.com>
2022-10-01 09:10:55 +0530

    day5up

--- clockifyBackend/src/index.js
+++ clockifyBackend/src/index.js
@@ -4,12 +4,14 @@ const connect = require("./config/db");
 
 const teamRouter = require("./Team/team.router");
 const projectRouter = require("./Project/project.router");
+const timeTracker= require("./TimeTracker/TimeTracker.router")
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/teams", teamRouter);
 app.use("/projects", projectRouter);
+app.use("/timetrackers",timeTracker)
 
 app.get("/", (req, res) => {
   res.send("<h1>hello</h1>");
